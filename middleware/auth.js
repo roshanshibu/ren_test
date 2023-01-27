f174ece3c86c52a11b7a0ba4ead3d9bf68c55955
roshan <roshanshibu97@gmail.com>
2023-01-27 16:29:21 +0100

    deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,6 +8,6 @@ module.exports = (req, res, next) => {
       next();
     } catch(error) {
       console.log(error)
-      res.status(400).json({ error: error }); 
+      res.status(401).json({ error: error }); 
     }
 };
